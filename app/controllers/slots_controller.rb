bdded92872e56ea48412f5a0bdaef3b884d419d0
Samuel BOHN <bohn.sjl@gmail.com>
2022-12-02 13:37:07 +0700

    added bookable_slots

--- app/controllers/slots_controller.rb
+++ app/controllers/slots_controller.rb
@@ -1,9 +1,46 @@
 class SlotsController < ApplicationController
   before_action :find_slot, only: [:show ]#:update, :destroy]
   def index
+    params.require(:start_at)
+    params.require(:end_at)
     params.permit(:start_at, :end_at)
-    @slots = Slot.all
+    start_at = Time.parse(params[:start_at])
+    end_at = Time.parse(params[:end_at])
+    @slots = Slot.where("end_at > ?", start_at).where("start_at <= ?", end_at).order(start_at: :asc)
     render json: @slots
+  end
+
+  def bookable_slots
+    params.require(:start_at)
+    params.require(:end_at)
+    params.require(:slot_duration)
+    params.permit(:start_at, :end_at, :slot_duration)
+    start_at = Time.parse(params[:start_at])
+    end_at = Time.parse(params[:end_at])
+    slot_duration = ActiveSupport::Duration.parse(params[:slot_duration]) # "P1Y2M10DT2H30M"
+    slot_increment = ActiveSupport::Duration.parse("PT15M")
+
+    # Generate all possible slots of a day
+    possible_slots = []
+    slot_cursor = start_at
+    while slot_cursor + slot_duration < end_at
+      possible_slots.push(Slot.new(start_at: slot_cursor, end_at: slot_cursor + slot_duration))
+      slot_cursor += slot_increment
+    end
+
+    # get existings slot of the day
+    slots = Slot.where("end_at > ?", start_at).where("start_at <= ?", end_at).order(start_at: :asc).to_a
+
+    #  remove unbookable slots
+    i = possible_slots.length - 1
+    while i >= 0
+      overlapping_slots = slots.select{|s| s.end_at >= possible_slots[i].start_at && s.start_at <= possible_slots[i].end_at}
+      bookable = overlapping_slots.count == 0
+      possible_slots.delete_at(i) unless bookable
+      i -= 1
+    end
+    render json: possible_slots
+
   end
 
   def create
